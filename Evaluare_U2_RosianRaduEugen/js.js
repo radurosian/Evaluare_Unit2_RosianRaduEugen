1ec53d9f6779a2ee0538a7df5a8c7be59b0e3dea
radurosian <73156422+radurosian@users.noreply.github.com>
2020-11-27 19:14:06 +0200

    Add files via upload

---
+++ Evaluare_U2_RosianRaduEugen/js.js
@@ -0,0 +1,14 @@
+var slideIndex = 0;
+
+
+function slideshow() {
+  var i;
+  var slides = document.getElementsByClassName("slide");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(slideshow, 3000);
+}
